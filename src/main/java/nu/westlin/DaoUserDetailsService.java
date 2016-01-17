7da4af58a8e25e478b279f07ea9857e33c0107fe
Peter Vestlin <peter.vestlin@lm.se>
2016-01-17 19:41:07 +0100

    Initial commit.

---
+++ src/main/java/nu/westlin/DaoUserDetailsService.java
@@ -0,0 +1,37 @@
+package nu.westlin;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DaoUserDetailsService implements UserDetailsService {
+
+    @Override public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        switch(username) {
+            case "user":
+                return new User("user", "password", createAuthorities("USER"));
+            case "sune":
+                return new User("sune", "sune", createAuthorities("SUNE"));
+            default:
+                return null;
+        }
+    }
+
+    private List<? extends GrantedAuthority> createAuthorities(String... roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+
+        return authorities;
+    }
+}
